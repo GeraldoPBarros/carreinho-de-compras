d3c4acfc45bc25c38045efa6fa3d52feaa8a4325
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-07-04 19:45:04 -0300

    adicao de quantidade de itens em header, listagem e exibicao dos produtos, handler da atualizacao de incremento e decremento dos produtos no carrinho.

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import {
   MdDelete,
   MdAddCircleOutline,
   MdRemoveCircleOutline,
-} from 'react-icons/md';
+} from "react-icons/md";
 
-// import { useCart } from '../../hooks/useCart';
-// import { formatPrice } from '../../util/format';
-import { Container, ProductTable, Total } from './styles';
+import { useCart } from "../../hooks/useCart";
+import { formatPrice } from "../../util/format";
+import { Container, ProductTable, Total } from "./styles";
 
 interface Product {
   id: number;
@@ -18,24 +18,34 @@ interface Product {
 }
 
 const Cart = (): JSX.Element => {
-  // const { cart, removeProduct, updateProductAmount } = useCart();
+  const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const cartFormatted = cart.map((product: Product) => ({
+    priceFormatted: product.price,
+    subTotal: product.price * product.amount,
+  }));
+  const total = formatPrice(
+    cart.reduce((sumTotal, product) => {
+      sumTotal += product.price * product.amount;
+
+      return sumTotal;
+    }, 0)
+  );
 
   function handleProductIncrement(product: Product) {
-    // TODO
+    const prod = {
+      productId: product.id,
+      amount: product.amount + 1,
+    };
+    updateProductAmount(prod);
   }
 
   function handleProductDecrement(product: Product) {
-    // TODO
+    const prod = {
+      productId: product.id,
+      amount: product.amount > 0 ? product.amount - 1 : 0,
+    };
+    updateProductAmount(prod);
   }
 
   function handleRemoveProduct(productId: number) {
@@ -57,7 +67,10 @@ const Cart = (): JSX.Element => {
         <tbody>
           <tr data-testid="product">
             <td>
-              <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
+              <img
+                src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
+                alt="Tênis de Caminhada Leve Confortável"
+              />
             </td>
             <td>
               <strong>Tênis de Caminhada Leve Confortável</strong>
@@ -68,8 +81,8 @@ const Cart = (): JSX.Element => {
                 <button
                   type="button"
                   data-testid="decrement-product"
-                // disabled={product.amount <= 1}
-                // onClick={() => handleProductDecrement()}
+                  // disabled={product.amount <= 1}
+                  // onClick={() => handleProductDecrement()}
                 >
                   <MdRemoveCircleOutline size={20} />
                 </button>
@@ -82,7 +95,7 @@ const Cart = (): JSX.Element => {
                 <button
                   type="button"
                   data-testid="increment-product"
-                // onClick={() => handleProductIncrement()}
+                  // onClick={() => handleProductIncrement()}
                 >
                   <MdAddCircleOutline size={20} />
                 </button>
@@ -95,7 +108,7 @@ const Cart = (): JSX.Element => {
               <button
                 type="button"
                 data-testid="remove-product"
-              // onClick={() => handleRemoveProduct(product.id)}
+                // onClick={() => handleRemoveProduct(product.id)}
               >
                 <MdDelete size={20} />
               </button>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
+import React, { useState, useEffect } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-import { ProductList } from './styles';
-import { api } from '../../services/api';
-import { formatPrice } from '../../util/format';
-import { useCart } from '../../hooks/useCart';
+import { ProductList } from "./styles";
+import { api } from "../../services/api";
+import { formatPrice } from "../../util/format";
+import { useCart } from "../../hooks/useCart";
 
 interface Product {
   id: number;
@@ -22,28 +22,85 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce(
+    (sumAmount, product) => {
+      switch (product.id) {
+        case 1:
+          sumAmount[1] += 1;
+          break;
+        case 2:
+          sumAmount[2] += 1;
+          break;
+        case 3:
+          sumAmount[3] += 1;
+          break;
+        case 4:
+          sumAmount[4] += 1;
+          break;
+        case 5:
+          sumAmount[5] += 1;
+          break;
+        case 6:
+          sumAmount[6] += 1;
+          break;
+      }
+
+      return sumAmount;
+    },
+    {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+    } as CartItemsAmount
+  );
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      api.get("products").then((response) => setProducts(response.data));
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
     <ProductList>
-      <li>
+      {products.map((product) => (
+        <li>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{formatPrice(product.price)}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {cartItemsAmount[product.id] || 0}
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
+};
+
+export default Home;
+
+/* 
+<li>
         <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
         <strong>Tênis de Caminhada Leve Confortável</strong>
         <span>R$ 179,90</span>
@@ -54,14 +111,11 @@ const Home = (): JSX.Element => {
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+            {/* {cartItemsAmount[product.id] || 0} 2
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
-};
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
 
-export default Home;
+*/
